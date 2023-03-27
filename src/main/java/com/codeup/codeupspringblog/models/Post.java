625aae530d50ace8971134fc7dbd396b8163ef3c
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-27 13:31:26 -0500

    Post model

---
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,24 @@
+package com.codeup.codeupspringblog.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 1024, nullable = false, unique = true)
+    private String title;
+
+    @Column(length = 1024, nullable = false)
+    private String description;
+}
