de2747dc6d570b5c635e98abb96ab91bee9dbe2c
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-30 10:52:23 -0500

    added update post

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -6,10 +6,12 @@ import com.codeup.codeupspringblog.models.Post;
 import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,30 +41,50 @@ public class PostController {
     @GetMapping("/{id}")
     @ResponseBody
     public String viewPost(@PathVariable("id") Long id) {
-        return "Viewing post with ID " + id;
+        Post originalPost = postDao.findById(id).get();
+
+        Post post = new Post(originalPost.getId(), originalPost.getTitle(), originalPost.getDescription(), originalPost.getUser());
+
+        return "Viewing post with ID " + id + ". Title: " + post.getTitle() + " Description: " + post.getDescription();
     }
 
     // GET /posts/create
     @GetMapping("/create")
-    public String createPostForm() {
+    public String createPostForm(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
     // POST /posts/create
     @PostMapping("/create")
-    public String createPost(@RequestParam String title, @RequestParam String description) {
-        // Logic for creating a new post
-        Post post = new Post();
-        post.setTitle(title);
-        post.setDescription(description);
+    public String createPost(@ModelAttribute Post post) {
+
 
         User user = userDao.findById(2L).get();
         post.setUser(user);
 
-
         postDao.save(post);
 
         return "redirect:/posts";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdatePost(@PathVariable("id") long id, Model model) {
+        Post post = postDao.findById(id).get();
+        model.addAttribute("post", post);
+        return "posts/update";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updatePost(@PathVariable("id") long id, @ModelAttribute Post post) {
+        Post originalPost = postDao.findById(id).get();
+        post.setId(id);
+        post.setUser(originalPost.getUser());
+
+        postDao.save(post);
+        return "redirect:/posts/" + id;
+
+
     }
 }
 
