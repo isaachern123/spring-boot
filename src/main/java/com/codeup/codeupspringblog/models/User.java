1c27ea5ac0b9a3e73103337eb888bbcd364bcdfd
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-04-04 11:47:54 -0500

    etc

--- src/main/java/com/codeup/codeupspringblog/models/User.java
+++ src/main/java/com/codeup/codeupspringblog/models/User.java
@@ -30,4 +30,11 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     @ToString.Exclude
     private List<Post> posts;
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
 }
